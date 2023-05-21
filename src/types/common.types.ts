45a1be476f97d6ecbe7909f386c6a27c9f213743
Dmitry Ivanov <dimannisik1@gmail.com>
2023-05-21 18:52:57 +0700

    feat: add types for data models

---
+++ src/types/common.types.ts
@@ -0,0 +1,46 @@
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+type Posts = Post[];
+
+type Comments = Comment[];
+
+type Users = User[];
+
+export type { Post, Posts, Comment, Comments, User, Users };
